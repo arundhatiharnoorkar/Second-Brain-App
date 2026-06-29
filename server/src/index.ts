eaf96d87aca32eca25f83307613cc48dfa3e86f2
arundhatiharnoorkar <arundhatiharnoorkar@gmail.com>
2026-06-29 23:42:38 +0530

    Added frontend and backend properly

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import noteRoutes from "./routes/noteRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/notes", noteRoutes);
+
+
+app.listen(5000, () => {
+  console.log("Server running");
+});
