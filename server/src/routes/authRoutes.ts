a2f72b555d122265030ebbfdfe4bf8ffe35dc92d
arundhatiharnoorkar <arundhatiharnoorkar@gmail.com>
2026-07-05 20:28:21 +0530

    Update authRoutes.ts

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.post("/google",googleLogin)
+
 router.get("/me", authMiddleware, (req, res) => {
     res.json({
       message: "Protected route accessed",
