e5efc36bd14c53b9335cc4ca5bde9b9f79ffa01f
arundhatiharnoorkar <arundhatiharnoorkar@gmail.com>
2026-07-06 21:39:41 +0530

    Add default route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import Signup from "./pages/Signup";
