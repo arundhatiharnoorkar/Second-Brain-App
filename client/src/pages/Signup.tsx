bf9d4a76cdfc4715b8f3d6978a4d35c0217c1862
arundhatiharnoorkar <arundhatiharnoorkar@gmail.com>
2026-07-07 17:19:29 +0530

    Fix validations again

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -10,34 +10,12 @@ function Signup() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const passwordChecks = {
-  length: password.length >= 8,
-  uppercase: /[A-Z]/.test(password),
-  lowercase: /[a-z]/.test(password),
-  number: /[0-9]/.test(password),
-  special: /[^A-Za-z0-9]/.test(password),
-};
-
-const isPasswordValid =
-  passwordChecks.length &&
-  passwordChecks.uppercase &&
-  passwordChecks.lowercase &&
-  passwordChecks.number &&
-  passwordChecks.special;
-
   const handleSignup = async () => {
-
-    if (!email.trim()){
-      setError("Email is required");
-      return;
-    }
-
-    if (!password.trim()){
-      setError("password is required");
-      return;
-    }
-
-
+if (!email.trim() || !password.trim()) {
+    setError("Email and password are required");
+    return;
+  }
+  
     try {
         console.log("button clicked");
       const response = await axios.post(
@@ -82,39 +60,11 @@ const isPasswordValid =
           className="bg-white text-gray-900 placeholder-gray-400 border border-gray-200 rounded-xl p-3 w-full mb-5 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:border-transparent transition"
         />
 
-        <div className="text-sm mt-2 space-y-1">
-  <p className={passwordChecks.length ? "text-green-600" : "text-gray-500"}>
-    {passwordChecks.length ? "✅" : "⭕"} At least 8 characters
-  </p>
-
-  <p className={passwordChecks.uppercase ? "text-green-600" : "text-gray-500"}>
-    {passwordChecks.uppercase ? "✅" : "⭕"} One uppercase letter
-  </p>
-
-  <p className={passwordChecks.lowercase ? "text-green-600" : "text-gray-500"}>
-    {passwordChecks.lowercase ? "✅" : "⭕"} One lowercase letter
-  </p>
-
-  <p className={passwordChecks.number ? "text-green-600" : "text-gray-500"}>
-    {passwordChecks.number ? "✅" : "⭕"} One number
-  </p>
-
-  <p className={passwordChecks.special ? "text-green-600" : "text-gray-500"}>
-    {passwordChecks.special ? "✅" : "⭕"} One special character
-  </p>
-</div>
-
-       <button
-  onClick={handleSignup}
-  disabled={!isPasswordValid}
-  className={`w-full py-2.5 rounded-xl font-semibold transition ${
-    isPasswordValid
-      ? "bg-indigo-600 hover:bg-indigo-700 text-white"
-      : "bg-gray-300 text-gray-500 cursor-not-allowed"
-  }`}
->
-  Signup
-</button>
+        <button onClick={handleSignup}
+          className="bg-indigo-600 hover:bg-indigo-700 active:bg-indigo-800 text-white w-full py-2.5 font-semibold rounded-xl transition shadow-md shadow-indigo-200"
+        >
+          Signup
+        </button>
 
         {error && (
   <p className="text-red-500">
